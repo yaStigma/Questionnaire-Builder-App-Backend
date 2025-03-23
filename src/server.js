dc8aee3a1d2f8360dce169546ff1a75743c572c1
Tanya Yenakieva <yastigma@gmail.com>
2025-03-23 22:13:39 +0200

    update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 // import pino from 'pino-http';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import { env } from './utils/env.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
